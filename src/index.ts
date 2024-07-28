6ef00ae90d170b1278db612009e11a638468825a
David Liu <david-khala@hotmail.com>
2024-07-29 06:35:56 +0800

    add Dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import {serve, type HttpBindings} from '@hono/node-server'
+import {Hono} from 'hono'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: HttpBindings }>()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+    return c.text('Hello Hono!')
 })
 
-const port = 3000
+const port = process.env.PORT || '3000'
 console.log(`Server is running on port ${port}`)
 
 serve({
-  fetch: app.fetch,
-  port
+    fetch: app.fetch,
+    port: parseInt(port)
 })
